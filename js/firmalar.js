b5626c185714cd7917b96f156808a4a1a918ec58
1986sec <y3456605@gmail.com>
2025-08-19 19:38:09 +0300

    Backend URL'i Render'a güncellendi: https://adaso-backend.onrender.com

--- js/firmalar.js
+++ js/firmalar.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://adaso-backend.onrender.com/api';
 
 async function apiRequest(endpoint, options = {}) {
     try {
--- js/ziyaretler.js
+++ js/ziyaretler.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://adaso-backend.onrender.com/api';
 let editingZiyaretId = null;
 let cachedZiyaretler = null;
 
